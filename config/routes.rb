85a773ae0cf00c57a1eb8e34030fa62a9c24097b
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-08 13:34:19 -0600

    [FEAT]: Implemented devise and config for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'api/auth'
 
-  # namespace :api do
-  #   defaults(format: :json) do
-  #     resources :users, only: %i[create show update destroy]
-  #   end
-  # end
+  namespace :api do
+    defaults(format: :json) do
+      resources :users, only: %i[create show update destroy]
+    end
+  end
 end
