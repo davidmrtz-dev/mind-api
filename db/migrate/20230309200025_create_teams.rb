a70970aa3b2e816179f2c9221552e444911344cb
David Mtz <davidmtz@Davids-MacBook-Pro.local>
2023-03-09 14:02:33 -0600

    [CHORE]: Updated migration file.

--- db/migrate/20230309200025_create_teams.rb
+++ db/migrate/20230309200025_create_teams.rb
@@ -1,7 +1,7 @@
 class CreateTeams < ActiveRecord::Migration[6.1]
   def change
     create_table :teams do |t|
-      t.string :name
+      t.string :name, null: false
       t.references :account, null: false, foreign_key: true
 
       t.timestamps
