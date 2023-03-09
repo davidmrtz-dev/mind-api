635df811f115c7750631ea4d0904f346860998c9
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-09 14:39:48 -0600

    [FEAT]: Implemented tests.

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Team, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to(:account) }
+    it { is_expected.to have_many(:users).through(:user_teams) }
   end
 
   describe 'validations' do
