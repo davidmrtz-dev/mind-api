ae5d93f91285514d414aa1d217b3f01b9d2d15bd
David Mtz <davidmtz@Davids-MacBook-Pro.local>
2023-03-09 14:08:18 -0600

    [FEAT]: Added validations and test for model.

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
+  describe 'associations' do
+    it { is_expected.to have_many(:teams).dependent(:destroy) }
+  end
+
   describe 'validations' do
     it { should allow_value('A client name').for(:client_name).on(:update) }
     it { should allow_value('Jhon Doe').for(:manager_name).on(:update) }
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:account) }
+  end
+
+  describe 'validations' do
+    it { should allow_value('Thundercats').for(:name).on(:update) }
+  end
 end
