7f9c9f43ed5df3bb9bc80e6777cd37c303249cd2
David Mtz <davidmtz@Davids-MacBook-Pro.local>
2023-03-09 14:24:50 -0600

    [FEAT]: Added migration for join model.

---
+++ db/migrate/20230309202157_create_user_teams.rb
@@ -0,0 +1,13 @@
+class CreateUserTeams < ActiveRecord::Migration[6.1]
+  def change
+    create_table :user_teams do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :team, null: false, foreign_key: true
+      t.date :start_at
+      t.date :end_at
+      t.integer :status, null: false, default: 'unactive'
+
+      t.timestamps
+    end
+  end
+end
