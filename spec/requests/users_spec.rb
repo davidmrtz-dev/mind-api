6731660a8ac938c5abb75d5e8ebe2de8ed1c280b
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-10 12:14:14 -0600

    [FEAT]: Updated helpers for tests.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Api::UsersController, type: :controller do
       get :index
 
       expect(response).to have_http_status(:ok)
+      expect(parsed_response[:users].map { |o| o[:id] }).to match_array(User.ids)
     end
   end
 end
--- spec/support/request.rb
+++ spec/support/request.rb
@@ -1,8 +1,8 @@
-def parsed_body
+def parsed_response
   body = JSON.parse(response.body)
 
   if body.instance_of?(Hash)
-    body.with_indefferente_access
+    body.with_indifferent_access
   elsif body.instance_of?(Array)
     body
   end
