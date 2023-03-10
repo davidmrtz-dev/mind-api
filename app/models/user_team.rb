ab2fc8473e02a6d86d2d4f0407eea88df1990290
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-10 11:32:25 -0600

    [CHORE]: Added seeeds for users and users teams.

--- app/models/user_team.rb
+++ app/models/user_team.rb
@@ -1,4 +1,6 @@
 class UserTeam < ApplicationRecord
   belongs_to :user
   belongs_to :team
+
+  enum status: { active: 0, inactive: 1 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@ account = Account.create!(
   name: 'Bussiness Account'
 )
 
+3.times.each do |num|
+  User.create!(
+    email: "user-#{num}@example.com",
+    password: 'password'
+  )
+end
+
 3.times do
   Team.create!(
     account: account,
@@ -13,3 +20,14 @@ account = Account.create!(
   )
 end
 
+Team.all.each do |team|
+  User.first(2).each do |user|\
+    UserTeam.create!(
+      team: team,
+      user: user,
+      status: :active
+    )
+  end
+end
+
+UserTeam.create!(team: Team.last, user: User.last, status: :active)
