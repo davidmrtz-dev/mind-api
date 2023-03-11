7f649ee439f64557214305cfa0472713904c045f
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-11 17:08:32 -0600

    [CHORE]: Implemented tests for show action.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe Api::UsersController, type: :controller do
       expect(parsed_response[:users].map { |o| o[:id] }).to match_array(User.ids)
     end
   end
+
+  xdescribe 'GET /api/user/:id' do
+    login_user
+
+    it 'returns a user' do
+      get :show, params: { id: 0 }
+
+      expect(response).to have_http_status(:ok)
+      expect(parsed_response[:user][:id]).to eq 0
+    end
+  end
 end
