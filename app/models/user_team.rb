45818db119ee2fff2c4add84c588bfa2fbf31609
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-13 16:15:09 -0600

    [FEAT]: Added controller and test suite

--- app/models/user_team.rb
+++ app/models/user_team.rb
@@ -2,5 +2,5 @@ class UserTeam < ApplicationRecord
   belongs_to :user
   belongs_to :team
 
-  enum status: { active: 0, inactive: 1 }
+  enum status: { active: 0, inactive: 1 }, _default: :inactive
 end
