40df149a1e8ae557e6d0b807e8b749dce48a2dc3
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-13 15:35:30 -0600

    [FEAT]: Added tests

--- spec/requests/teams_spec.rb
+++ spec/requests/teams_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe Api::TeamsController, type: :controller do
       expect(parsed_response[:teams].map { |t| t[:id] }).to match_array(Team.ids)
     end
   end
+
+  describe 'GET /api/teams/:id' do
+    let(:account) { AccountFactory.create }
+    let(:team) { TeamFactory.create(account: account) }
+
+    it 'returns an account' do
+      get :show, params: { id: team.id }
+
+      expect(response).to have_http_status(:ok)
+      expect(parsed_response[:team][:id]).to eq team.id
+    end
+  end
 end
