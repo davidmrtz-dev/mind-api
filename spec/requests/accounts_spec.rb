dcf0e2cf64344e5c59243b5bba56fa0ae4d2440a
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-13 13:11:26 -0600

    [WIP]: Added tests

--- spec/requests/accounts_spec.rb
+++ spec/requests/accounts_spec.rb
@@ -99,4 +99,25 @@ RSpec.describe Api::AccountsController, type: :controller do
       expect(response).to have_http_status(:unprocessable_entity)
     end
   end
+
+  describe 'DELETE /api/accounts/:id' do
+    let(:account) { UserFactory.create }
+
+    subject(:action) { delete :destroy, params: { id: account.id } }
+
+    login_user
+
+    it 'calls to delete the user' do
+      expect { action }.to change { Account.count }.by (-1)
+
+      action
+
+      expect(response).to have_http_status(:no_content)
+    end
+
+    it 'handles not found' do
+      expect { delete :destroy, params: { id: 0 } }
+        .to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
 end
