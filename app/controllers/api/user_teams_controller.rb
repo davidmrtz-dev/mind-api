c13be033f6c498bc95932a084d8a338e62499c48
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-14 12:25:09 -0600

    [FEAT]: Implemented index action

--- app/controllers/api/user_teams_controller.rb
+++ app/controllers/api/user_teams_controller.rb
@@ -1,4 +1,21 @@
 module Api
   class UserTeamsController < ApiController
+    include Pagination
+
+    before_action :authenticate_user!
+
+    def index
+      user_teams = UserTeam.all
+
+      page = paginate(
+        user_teams,
+        limit: params[:limit],
+        offset: params[:offset]
+      )
+
+      render json: {
+        user_teams: ::Api::UserTeamsSerializer.json(page)
+      }
+    end
   end
 end
