0f9393025d47efdb835e04a35743e9ec1c4e049d
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-14 13:25:07 -0600

    [FEAT]: Implmented validation for end_at property

--- spec/models/user_team_spec.rb
+++ spec/models/user_team_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe UserTeam, type: :model do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:team) }
   end
+
+  describe 'validations' do
+    let!(:user) { UserFactory.create(password: 'password') }
+    let!(:account) { AccountFactory.create }
+    let!(:team) { TeamFactory.create(account: account) }
+
+    it 'should not allow an end_date before today' do
+      user_team = UserTeam.new(user: user, team: team, status: :inactive, end_at: Time.zone.now - 5.days)
+      expect(user_team.valid?).to be_falsey
+      expect(user_team.errors.full_messages).to include('End date cannot be before today')
+    end
+  end
 end
