ed85ed863218aa23e215a7b8d1b74244a9d7a95a
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-14 12:39:55 -0600

    [WIP]: Added tests for action

--- spec/requests/user_teams_spec.rb
+++ spec/requests/user_teams_spec.rb
@@ -5,14 +5,55 @@ RSpec.describe Api::UserTeamsController, type: :controller do
   let(:account) { AccountFactory.create }
   let(:team) { TeamFactory.create(account: account) }
 
-  login_user
+  describe 'GET /api/user_teams' do
+    login_user
 
-  it 'returns the user teams realations' do
-    3.times { UserTeamFactory.create(user: user, team: team, status: :active)  }
+    it 'returns the user teams realations' do
+      3.times { UserTeamFactory.create(user: user, team: team, status: :active)  }
 
-    get :index
+      get :index
 
-    expect(response).to have_http_status(:ok)
-    expect(parsed_response[:user_teams].map { |u| u[:id] }).to match_array(UserTeam.ids)
+      expect(response).to have_http_status(:ok)
+      expect(parsed_response[:user_teams].map { |u| u[:id] }).to match_array(UserTeam.ids)
+    end
+  end
+
+  describe 'POST /api/user_teams' do
+    subject(:action) {
+      post :create, params: {
+        user_team: {
+          user_id: user.id,
+          team_id: team.id,
+          start_at: Date.today,
+          end_date: Date.tomorrow,
+          status: 'active'
+        }
+      }
+    }
+
+    login_user
+
+    it 'creates and return a user_team' do
+      expect { action }.to change { UserTeam.count }.by 1
+
+      action
+
+      user_team = UserTeam.last
+
+      expect(response).to have_http_status(:created)
+      expect(parsed_response[:user_team][:id]).to eq user_team.id
+      expect(parsed_response[:user_team][:user_id]).to eq user.id
+      expect(parsed_response[:user_team][:team_id]).to eq team.id
+    end
+
+    it 'handles validation error' do
+      post :create, params: {
+        user_team: {
+          user_id: nil
+        }
+      }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
   end
 end
