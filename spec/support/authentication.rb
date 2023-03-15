fcd4cf0b992dbf20988dd0dc5392af58908d7a37
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-14 21:13:01 -0600

    [FEAT]: Updated helper and tests to pass

--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -1,6 +1,6 @@
 def login_user
   before(:each) do
-    user = User.last.presence || User.create!(email: 'user@example.com', password: 'password', user_type: :admin)
+    user = User.last.presence || User.create!(email: 'user@example.com', password: 'password')
     headers = user.create_new_auth_token
     request.headers['access-token'] = headers['access-token']
     request.headers['client'] = headers['client']
