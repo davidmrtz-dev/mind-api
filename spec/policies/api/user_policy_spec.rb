1ba4cc4cc7f48bf6644f85e4d62d8cc99d67f5b9
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-14 19:26:14 -0600

    [FEAT]: Implemented tests for users policies

--- spec/policies/api/user_policy_spec.rb
+++ spec/policies/api/user_policy_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Api::UserPolicy, type: :policy do
   context 'when user is standard' do
     let(:user) { UserFactory.create(password: 'password') }
-    let(:context) { { user: user } }
+    let(:other_user) { UserFactory.create(password: 'password') }
+    let(:context) { { user: other_user } }
     let(:user_policy) { described_class.new(user, context) }
 
     shared_examples_for 'not authorized standard user' do
@@ -61,6 +62,73 @@ RSpec.describe Api::UserPolicy, type: :policy do
       subject { user_policy.apply(:index?) }
 
       include_examples 'authorized admin user'
+    end
+
+    describe '#show' do
+      subject { user_policy.apply(:show?) }
+
+      include_examples 'authorized admin user'
+    end
+
+    describe '#create' do
+      subject { user_policy.apply(:create?) }
+
+      include_examples 'authorized admin user'
+    end
+
+    describe '#update' do
+      subject { user_policy.apply(:update?) }
+
+      include_examples 'authorized admin user'
+    end
+
+    describe '#destroy' do
+      subject { user_policy.apply(:destroy?) }
+
+      include_examples 'authorized admin user'
+    end
+  end
+
+  context 'when user is admin' do
+    let(:user) { UserFactory.create(password: 'password') }
+    let(:super_user) { UserFactory.create(password: 'password', user_type: :super) }
+    let(:context) { { user: super_user } }
+    let(:user_policy) { described_class.new(user, context) }
+
+    shared_examples_for 'authorized super user' do
+      it 'returns true when user is super' do
+        is_expected.to be_truthy
+      end
+    end
+
+    describe '#index' do
+      subject { user_policy.apply(:index?) }
+
+      include_examples 'authorized super user'
+    end
+
+    describe '#show' do
+      subject { user_policy.apply(:show?) }
+
+      include_examples 'authorized super user'
+    end
+
+    describe '#create' do
+      subject { user_policy.apply(:create?) }
+
+      include_examples 'authorized super user'
+    end
+
+    describe '#update' do
+      subject { user_policy.apply(:update?) }
+
+      include_examples 'authorized super user'
+    end
+
+    describe '#destroy' do
+      subject { user_policy.apply(:destroy?) }
+
+      include_examples 'authorized super user'
     end
   end
 end
