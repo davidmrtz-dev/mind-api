5c270aa58d0a0bb88e97ddfdaa6a588c38cb657b
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-14 19:41:14 -0600

    [FEAT]: Applied policies to users controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,6 @@
 class ApiController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  include ActionPolicy::Controller
+
+  authorize :user, through: :current_user
 end
