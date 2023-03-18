cb42f8cc5b3f0d43c1a3bb2c4181a34e7a77a173
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-18 15:01:00 -0600

    [CHORE]: Fixed accounts get action

--- spec/requests/api/v1/accounts_spec.rb
+++ spec/requests/api/v1/accounts_spec.rb
@@ -5,32 +5,40 @@ RSpec.describe 'api/v1/accounts', type: :request do
   path '/api/v1/accounts' do
 
     get('list accounts') do
-      response(200, 'successful') do
+      tags 'Accounts'
+      security ['access-token':[], client: [], uid: []]
+      parameter name: 'access-token', in: :header, type: :string
+      parameter name: 'client', in: :header, type: :string
+      parameter name: 'uid', in: :header, type: :string
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+      response '200', 'Accounts Retrieved' do
+        let!(:account) { AccountFactory.create }
+        let!(:user) { UserFactory.create(user_type: :admin) }
+        let!(:hdrs) { user.create_new_auth_token }
+        let(:'access-token') { hdrs['access-token'] }
+        let(:client) { hdrs['client']}
+        let(:uid) { hdrs['uid'] }
+
+        run_test! do |response|
+          expect(response).to have_http_status(:ok)
+          expect(parsed_response[:accounts].first[:id]).to eq account.id
         end
-        run_test!
       end
     end
 
-    post('create account') do
-      response(200, 'successful') do
+    # post('create account') do
+    #   response(200, 'successful') do
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
+    #     after do |example|
+    #       example.metadata[:response][:content] = {
+    #         'application/json' => {
+    #           example: JSON.parse(response.body, symbolize_names: true)
+    #         }
+    #       }
+    #     end
+    #     run_test!
+    #   end
+    # end
   end
 
   path '/api/v1/accounts/{id}' do
