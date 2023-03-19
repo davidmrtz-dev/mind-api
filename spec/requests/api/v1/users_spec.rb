2cc10f6b4c861d95e04d4566311e103d6b668a83
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-18 19:14:47 -0600

    [FIX]: Fixed index action for users resources

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,20 +1,27 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/users', type: :request do
-
   path '/api/v1/users' do
+    get('Retrieves users') do
+      tags 'Users'
+      consumes 'application/json'
+      security ['access-token':[], client: [], uid: []]
+      parameter name: 'access-token', in: :header, type: :string
+      parameter name: 'client', in: :header, type: :string
+      parameter name: 'uid', in: :header, type: :string
 
-    get('list users') do
-      response(200, 'successful') do
+      response '200', 'Accounts retrieved' do
+        let!(:user) { UserFactory.create(user_type: :admin) }
+        let!(:another_user) { UserFactory.create }
+        let!(:hdrs) { user.create_new_auth_token }
+        let(:'access-token') { hdrs['access-token'] }
+        let(:client) { hdrs['client']}
+        let(:uid) { hdrs['uid'] }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+        run_test! do |response|
+          expect(response).to have_http_status(:ok)
+          expect(parsed_response[:users].first[:id]).to eq another_user.id
         end
-        run_test!
       end
     end
 
