419077127696d366fa3bacbb84a89612412f755d
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-18 22:52:25 -0600

    [FIX]: Fixed create

--- spec/requests/api/v1/user_teams_spec.rb
+++ spec/requests/api/v1/user_teams_spec.rb
@@ -29,17 +29,58 @@ RSpec.describe 'api/v1/user_teams', type: :request do
       end
     end
 
-    post('create user_team') do
-      response(200, 'successful') do
+    post('Creates user_team') do
+      tags 'UserTeams'
+      consumes 'application/json'
+      security ['access-token':[], client: [], uid: []]
+      parameter name: 'access-token', in: :header, type: :string
+      parameter name: 'client', in: :header, type: :string
+      parameter name: 'uid', in: :header, type: :string
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
+      parameter name: :params, in: :body, schema: {
+        type: :object,
+        properties: {
+          user_team: {
+            type: :object,
+            properties: {
+              user_id: { type: :number },
+              team_id: { type: :number },
+              start_at: { type: :string },
+              end_at: { type: :string },
+              status: { type: :string }
+            },
+            required: %w(user_id team_id start_at end_at status)
+          }
+        },
+        required: %(user_team)
+      }
+
+      response '201', 'Team created' do
+        let!(:account) { AccountFactory.create }
+        let!(:team) { TeamFactory.create(account: account) }
+        let!(:user) { UserFactory.create }
+        let!(:admin) { UserFactory.create(user_type: :admin) }
+        let!(:hdrs) { admin.create_new_auth_token }
+        let(:'access-token') { hdrs['access-token'] }
+        let(:client) { hdrs['client']}
+        let(:uid) { hdrs['uid'] }
+
+        let!(:params) do
+          {
+            user_team: {
+              user_id: user.id,
+              team_id: team.id,
+              start_at: Time.zone.today,
+              end_at: Time.zone.tomorrow,
+              status: 'active'
             }
           }
         end
-        run_test!
+
+        run_test! do |response|
+          expect(response).to have_http_status(:created)
+          expect(parsed_response[:user_team][:id]).to eq UserTeam.first.id
+        end
       end
     end
   end
