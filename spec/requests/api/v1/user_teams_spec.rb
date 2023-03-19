9a7a05d5b74b84fa52d2fc00fe0ab6d87887793c
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-18 22:57:04 -0600

    [FIX]: Fixed update

--- spec/requests/api/v1/user_teams_spec.rb
+++ spec/requests/api/v1/user_teams_spec.rb
@@ -86,36 +86,59 @@ RSpec.describe 'api/v1/user_teams', type: :request do
   end
 
   path '/api/v1/user_teams/{id}' do
-    # You'll want to customize the parameter types...
-    parameter name: 'id', in: :path, type: :string, description: 'id'
+    put('Updates a user_team') do
+      tags 'UserTeams'
+      consumes 'application/json'
+      security ['access-token':[], client: [], uid: []]
+      parameter name: 'id', in: :path, type: :string, description: 'id'
+      parameter name: 'access-token', in: :header, type: :string
+      parameter name: 'client', in: :header, type: :string
+      parameter name: 'uid', in: :header, type: :string
 
-    patch('update user_team') do
-      response(200, 'successful') do
-        let(:id) { '123' }
+      parameter name: :params, in: :body, schema: {
+        type: :object,
+        properties: {
+          user_team: {
+            type: :object,
+            properties: {
+              user_id: { type: :number },
+              team_id: { type: :number },
+              start_at: { type: :string },
+              end_at: { type: :string },
+              status: { type: :string }
+            },
+            required: %w(user_id team_id start_at end_at status)
+          }
+        },
+        required: %(user_team)
+      }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
+      response '200', 'Team updated' do
+        let!(:account) { AccountFactory.create }
+        let!(:team) { TeamFactory.create(account: account) }
+        let!(:user) { UserFactory.create }
+        let!(:user_team) { UserTeamFactory.create(user: user, team: team, status: :inactive) }
+        let!(:admin) { UserFactory.create(user_type: :admin) }
+        let!(:hdrs) { admin.create_new_auth_token }
+        let(:'access-token') { hdrs['access-token'] }
+        let(:client) { hdrs['client']}
+        let(:uid) { hdrs['uid'] }
+        let(:id) { user_team.id }
+        let!(:params) do
+          {
+            user_team: {
+              start_at: Time.zone.today,
+              end_at: Time.zone.tomorrow,
+              status: 'active'
             }
           }
         end
-        run_test!
-      end
-    end
 
-    put('update user_team') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+        run_test! do |response|
+          expect(response).to have_http_status(:ok)
+          expect(parsed_response[:user_team][:id]).to eq user_team.id
+          expect(parsed_response[:user_team][:status]).to eq 'active'
         end
-        run_test!
       end
     end
 
