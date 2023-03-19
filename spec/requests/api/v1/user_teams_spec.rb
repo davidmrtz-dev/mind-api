e60556922f59d0f23d1cbea80bb05240195441fa
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-18 22:48:23 -0600

    [FIX]: Fixed index

--- spec/requests/api/v1/user_teams_spec.rb
+++ spec/requests/api/v1/user_teams_spec.rb
@@ -3,18 +3,29 @@ require 'swagger_helper'
 RSpec.describe 'api/v1/user_teams', type: :request do
 
   path '/api/v1/user_teams' do
+    get('Retrieves user_teams') do
+      tags 'UserTeams'
+      consumes 'application/json'
+      security ['access-token':[], client: [], uid: []]
+      parameter name: 'access-token', in: :header, type: :string
+      parameter name: 'client', in: :header, type: :string
+      parameter name: 'uid', in: :header, type: :string
 
-    get('list user_teams') do
-      response(200, 'successful') do
+      response '200', 'Teams retrieved' do
+        let!(:account) { AccountFactory.create }
+        let!(:team) { TeamFactory.create(account: account) }
+        let!(:user) { UserFactory.create }
+        let!(:user_team) { UserTeamFactory.create(user: user, team: team) }
+        let!(:admin) { UserFactory.create(user_type: :admin) }
+        let!(:hdrs) { admin.create_new_auth_token }
+        let(:'access-token') { hdrs['access-token'] }
+        let(:client) { hdrs['client']}
+        let(:uid) { hdrs['uid'] }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+        run_test! do |response|
+          expect(response).to have_http_status(:ok)
+          expect(parsed_response[:user_teams].first[:id]).to eq user_team.id
         end
-        run_test!
       end
     end
 
