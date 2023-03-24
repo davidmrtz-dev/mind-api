66b5ccf6735df11d4314fd5c9926813b82316ce1
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-24 12:54:03 -0600

    [CHORE]: Updated dependent relation between users and teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,7 @@
 class Team < ApplicationRecord
   belongs_to :account
 
-  has_many :user_teams
+  has_many :user_teams, dependent: :destroy
   has_many :users, through: :user_teams
 
   validates :name, presence: true
