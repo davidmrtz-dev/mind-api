3ce59ca411cfa3cd933e722e2666d9869707e2a6
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-28 22:29:41 -0600

    [CHORE]: Updated user serializator to meet match requirements.

--- app/serializers/api/users_serializer.rb
+++ app/serializers/api/users_serializer.rb
@@ -10,16 +10,22 @@ module Api
 
     def json
       @users.map do |user|
-        user.serializable_hash(
-          include: %i[
-            profile
-            teams
-          ],
-          except: %i[
-            created_at
-            updated_at
-          ]
-        )
+        teams = user.teams.includes(:user_teams).map do |team|
+          user_team = team.user_teams.find_by(user_id: user.id)
+          team.as_json(except: %i[created_at updated_at]).merge(user_team: user_team.as_json(except: %i[created_at updated_at]))
+        end
+
+        {
+          id: user.id,
+          name: user.name,
+          nickname: user.nickname,
+          email: user.email,
+          user_type: user.user_type,
+          profile: user.profile.serializable_hash(
+            except: %i[created_at updated_at]
+          ),
+          teams: teams
+        }
       end
     end
   end
