c5e4000c073d191d2b2ba4da6c3bbced28511696
davidmrtz-dev <davidelimg1@gmail.com>
2023-04-06 15:06:08 -0600

    [WIP]: Added test for search by dates range.

--- spec/lib/query/teams_search_service_spec.rb
+++ spec/lib/query/teams_search_service_spec.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 require 'query/teams_search_service'
 
 describe Query::TeamSearchService do
-  let!(:today) { Time.zone.today }
   let(:user) { UserFactory.create(password: 'password') }
   let(:account) { AccountFactory.create }
   let!(:team_a) { TeamFactory.create(account: account, name: 'Lincers') }
   let!(:team_b) { TeamFactory.create(account: account, name: 'Alligators') }
+  let!(:user_team_a) { UserTeamFactory.create(user: user, team: team_a, start_at: 2.days.ago, end_at: Time.zone.tomorrow)}
+  let!(:user_team_b) { UserTeamFactory.create(user: user, team: team_b, start_at: 2.days.from_now, end_at: 5.days.from_now) }
 
   context 'when params are not valid' do
     it 'should raise an error' do
       expect do
         described_class.for(user, {
           keyword: 'Foxes',
-          start_at: today.days_ago(2).to_s,
+          start_at: 2.days.ago,
           end_at: ''
         })
       end.to raise_error(Errors::InvalidParameters)
@@ -30,6 +31,21 @@ describe Query::TeamSearchService do
         })
 
         expect(result.count).to eq 1
+        expect(result.first.id).to eq team_a.id
+        expect(result.first.name).to eq team_a.name
+      end
+    end
+
+    describe 'when dates params are provided but not keyword' do
+      it 'should return matching teams based on dates range' do
+        result = described_class.for(Team.all, {
+          keyword: '',
+          start_at: 2.days.ago,
+          end_at: Time.zone.tomorrow
+        })
+
+        expect(result.count).to eq 1
+        expect(result.first.id).to eq team_a.id
         expect(result.first.name).to eq team_a.name
       end
     end
