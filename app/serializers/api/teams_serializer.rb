1d978e5b05d483d85dfb5318a344a824e36ff39b
davidmrtz-dev <davidelimg1@gmail.com>
2023-04-06 11:22:00 -0600

    [FEAT]: Extended behavior of teams serializer.

--- app/serializers/api/teams_serializer.rb
+++ app/serializers/api/teams_serializer.rb
@@ -1,12 +1,12 @@
 module Api
   class TeamsSerializer
-    def initialize(teams, user_id = nil)
+    def initialize(teams, user_id)
       @teams = teams
       @user_id = user_id
     end
 
-    def self.json(teams)
-      new(teams).json
+    def self.json(teams, user_id = nil)
+      new(teams, user_id).json
     end
 
     def json
@@ -21,7 +21,12 @@ module Api
           ]
         )
 
-        return serialized.merge({ ok: 'ok' }) if @user_id.present?
+        if @user_id.present?
+          user_team = team.user_teams.find_by(user_id: @user_id)
+          serialized.merge!(user_team: user_team&.serializable_hash(
+            except: %i[created_at updated_at]
+          ))
+        end
 
         serialized
       end
